57c2aaebec82a5ddab208b9f537175c89c392970
Amanda Fagundes de Paula <amanda.fagundes096@gmail.com>
2020-04-20 21:34:41 -0300

    ex3 finished

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -30,26 +30,5 @@ H3 = 1 ./ (1 + exp(-Z3));
 
 % Gets the index with the largest value (indicating the probabilty of the example belongs to that class)
 [_,p] = max(H3, [], 2);
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
 
 end
